32d9ab7c1d59fe87ce71501596186356d5d8ef96
Robert <rheinemann97@gmail.com>
2019-12-25 17:36:28 +0100

    API Build 50%

--- 01_m2m_asyncapi.py
+++ 01_m2m_asyncapi.py
@@ -6,27 +6,48 @@ import fileinput
 from rdflib import OWL, RDFS, Namespace
 from collections import defaultdict
 
-# Import MQTT_Ontology
+# import mqtt enhanced ontology defined in mqttwotdl.ttl
 IN = 'mqttwotdl.ttl'
 WOTDL = Namespace('http://vsr.informatik.tu-chemnitz.de/projects/2019/growth/wotdl#')
 
 instance = rdflib.Graph()
 instance.parse(IN, format='n3')
 
-
-
-# extract mqttCommunicaiton information
-find_mqtt_requests = """SELECT ?d ?device ?mqtt_request ?name ?message ?endpoint ?sub
+# extract mqttCommunicaiton information for subscriptions
+find_mqtt_subs = """SELECT ?d ?device ?mqtt_request ?name ?message ?endpoint ?sub  
        WHERE {
             ?d a ?device_subclass.
             ?device_subclass a owl:Class.
             ?device_subclass rdfs:subClassOf wotdl:Device.
             OPTIONAL{ ?d wotdl:name ?device }
-            ?mqtt_request a wotdl:MqttCommunication .
-            ?mqtt_request wotdl:subscribesTo ?sub .
+            ?mqtt_request a wotdl:MqttCommunication.           
             OPTIONAL{?mqtt_request wotdl:name ?name}
-            ?mqtt_request wotdl:mqttMessage ?message . 
-            ?mqtt_request wotdl:mqttEndpoint ?endpoint .
+            ?mqtt_request wotdl:mqttMessage ?message. 
+            ?mqtt_request wotdl:mqttEndpoint ?endpoint.
+            ?mqtt_request wotdl:subscribesTo ?sub. 
+            OPTIONAL{?mqtt_request wotdl:mqttMessage ?message}
+            {
+                ?d wotdl:hasTransition ?t.
+                ?t wotdl:hasActuation ?mqtt_request.
+            } 
+            UNION
+            {
+                ?d wotdl:hasMeasurement ?mqtt_request.    
+            }
+        }
+"""
+# extract mqttCommunicaiton information for publishes
+find_mqtt_pubs = """SELECT ?d ?device ?mqtt_request ?name ?message ?endpoint ?pub  
+       WHERE {
+            ?d a ?device_subclass.
+            ?device_subclass a owl:Class.
+            ?device_subclass rdfs:subClassOf wotdl:Device.
+            OPTIONAL{ ?d wotdl:name ?device }
+            ?mqtt_request a wotdl:MqttCommunication.           
+            OPTIONAL{?mqtt_request wotdl:name ?name}
+            ?mqtt_request wotdl:mqttMessage ?message. 
+            ?mqtt_request wotdl:mqttEndpoint ?endpoint.
+            ?mqtt_request wotdl:publishesOn ?pub. 
             OPTIONAL{?mqtt_request wotdl:mqttMessage ?message}
             {
                 ?d wotdl:hasTransition ?t.
@@ -39,29 +60,32 @@ find_mqtt_requests = """SELECT ?d ?device ?mqtt_request ?name ?message ?endpoint
         }
 """
 
-# paths = channels ? -> fill with light/heat/fan 'categories' from ontology
+# openapi: paths => asyncapi: channels -> respresents api body
 channels = defaultdict(dict)
-mqtt_requests = instance.query(find_mqtt_requests, initNs={'wotdl': WOTDL, 'rdfs': RDFS, 'owl': OWL})
+# invoke SPARQL Query
+mqtt_subs = instance.query(find_mqtt_subs, initNs={'wotdl': WOTDL, 'rdfs': RDFS, 'owl': OWL})
+mqtt_pubs = instance.query(find_mqtt_pubs, initNs={'wotdl': WOTDL, 'rdfs': RDFS, 'owl': OWL})
 
-# resources -> describes device(id + schema[properties(speed), required props etc]) & operations (increase, decrease temp etc)
+
+# resources dictionary for describing api tree-structure based on key-values: {sub/pub endpoint: pub/sub-indicator...}
 resources = defaultdict(list)
 
-# fill resource dict
-for device, devicename, mqtt_request, name, message, endpoint, sub in mqtt_requests:
-    print('Device: %s MQTTReq: %s FName: %s Message: %s Endpoint: %s SubcribesTo: %s' % (device, mqtt_request, name,  message, endpoint, sub))
-#    resources[endpoint].append(
-#        {'subscribesTo' : subscribe.lower(), 'publishesOn' : publish.lower(), 'device' : devicename, 'name' : name,
-#         'message' : message})
-#    print('PARAMETERS FOR TEMPERATURE: ' + resources['temperature'])
+# fill resource dict -> actuators
+for device, devicename, mqtt_request, name, message, endpoint, sub in mqtt_subs:
+    print('Device: %s MQTTReq: %s FName: %s Message: %s Endpoint: %s SubscribesTo: %s' % (device, mqtt_request, name,  message, endpoint, sub))
+    resources[str(sub)].append({'device' : devicename, 'name' : name, 'message' : message, 'subscribesTo': sub})
 
+# fill resource dict -> sensors
+for device, devicename, mqtt_request, name, message, endpoint, pub in mqtt_pubs:
+    print('Device: %s MQTTReq: %s FName: %s Message: %s Endpoint: %s PublishesOn: %s' % (device, mqtt_request, name,  message, endpoint, pub))
+    resources[str(pub)].append({'device' : devicename, 'name' : name, 'message' : message, 'publishesOn': pub})
 
-
+# build api body - loop through resouces-entries and extract required parameter
 for resource in resources:
-	# read out resource -> contains a path element (/fan + operations...)
     path_params = [p for p in resource.split('/')[1:] if p[0] == '{' and p[-1] == '}']
     requests = resources[resource]
     channels[str(resource)] = {}
-    responses = {}
+    news = {}
 
     for request in requests:
         entry = {
@@ -71,30 +95,30 @@ for resource in resources:
         parameters = []
         add_parameters = False
 
-        if request['query_params'] != '':
-            query_params = [q.split('=')[0] for q in request['query_params'].split('&')]
-            parameters += [{'in': 'query', 'name': p, 'schema': {'type': 'string'}} for p in query_params]
-            add_parameters = True
+        #if request['query_params'] != '':
+        #    query_params = [q.split('=')[0] for q in request['query_params'].split('&')]
+        #    parameters += [{'in': 'query', 'name': p, 'schema': {'type': 'string'}} for p in query_params]
+        #    add_parameters = True
 
-        if len(path_params) > 0:
-            parameters += [{'in': 'path', 'name': p[1:-1], 'required': True, 'schema': {'type': 'string'}} for p in path_params]
-            add_parameters = True
+        #if len(path_params) > 0:
+        #    parameters += [{'in': 'path', 'name': p[1:-1], 'required': True, 'schema': {'type': 'string'}} for p in path_params]
+        #    add_parameters = True
 
-        if add_parameters:
-            entry['parameters'] = parameters
+        #if add_parameters:
+        #    entry['parameters'] = parameters
 
-        if request['message'] != None:
-            entry['payload'] = yaml.load(str(request['message']))
+        #if request['body'] != None:
+        #    entry['requestBody'] = yaml.load(str(request['body']))
+        for param in request:
+            if (str(param) == 'subscribesTo'):
+                # enhance response body
+                news['payload'] = {'type': 'object'}
+            #elif request['method'] == 'post':
+            ##    responses['201'] = {'description': 'Created'}
 
-        #ToDO: method -> pub and sub difference -> introduce both, check which is empty!
-        #if request['method'] == 'get':
-        #    responses['200'] = {'description': 'OK'}
-        #elif request['method'] == 'post':
-        #    responses['201'] = {'description': 'Created'}
-
-        entry['responses'] = responses
-
-        channels[str(resource)][str(request['method'])] = entry
+        entry['message'] = news
+        print(str(entry))
+        channels[str(resource)] = entry
 
 
 # create asyncapi overhead for file
